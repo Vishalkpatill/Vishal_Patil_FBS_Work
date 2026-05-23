10f02db9e4980d1695d3fd4ff5613a1b5df615e4
Vishalkpatill <vishalpatil0939@gmail.com>
2026-05-23 13:49:20 +0530

    Add files via upload

---
+++ ASSIGNMENT_3/Q4.py
@@ -0,0 +1,12 @@
+## Write a program to input all sides of a triangle and check whether triangle is valid or not
+
+side1 = int(input('Enter side 1 : '))
+side2 = int(input('Enter side 2 : '))
+side3 = int(input('Enter side 3 : '))
+
+if (side1 + side2) > side3 and (side2 + side3) > side1 and (side3 + side1) > side2 :
+    print('triangle is valid')
+else :
+    print('triangle is not valid')            
+
+
