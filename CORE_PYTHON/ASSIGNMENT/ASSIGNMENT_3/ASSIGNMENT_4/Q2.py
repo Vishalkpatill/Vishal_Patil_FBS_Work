21563ad3b7b6cc1e68dce72de0e2a343133006be
Vishalkpatill <vishalpatil0939@gmail.com>
2026-05-28 22:53:36 +0530

    Add files via upload

---
+++ CORE_PYTHON/ASSIGNMENT/ASSIGNMENT_3/ASSIGNMENT_4/Q2.py
@@ -0,0 +1,10 @@
+## 2. WAP to print all odd numbers until n.
+
+num = int(input('enter number : '))
+
+i = 1
+
+while(i <= num):
+    print(i)
+
+    i += 2
---
+++ CORE_PYTHON/ASSIGNMENT/ASSIGNMENT_3/ASSIGNMENT_4/Q3.py
@@ -0,0 +1,12 @@
+## WAP to print sum of series upto n.
+
+n = int(input('enter number : '))
+
+i = 1
+sum = 0
+
+while(i <= n):
+    sum = sum + i
+    print(sum)
+
+    i += 1
